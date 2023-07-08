27b9c3b800f6d9b3c7fc5f7ffca3e3eb4a531f62
Barca545 <124243414+Barca545@users.noreply.github.com>
2023-07-07 21:45:56 -0400

    flipped the direction of the red bans

--- client/src/Components/RedBans.tsx
+++ client/src/Components/RedBans.tsx
@@ -22,19 +22,19 @@ export const RedBans = () => {
     return(
       <div className='red-bans'>
         <span className='ban-image-wrapper'>
-          <img className='ban-image' src={draft.redBans[0].icon} alt=''/>
-        </span>
-        <span className='ban-image-wrapper'>
-          <img className='ban-image' src={draft.redBans[1].icon} alt=''/>
-        </span>
-        <span className='ban-image-wrapper'>
-          <img className='ban-image' src={draft.redBans[2].icon} alt=''/>
+          <img className='ban-image' src={draft.redBans[4].icon} alt=''/>
         </span>
         <span className='ban-image-wrapper'>
           <img className='ban-image' src={draft.redBans[3].icon} alt=''/>
         </span>
         <span className='ban-image-wrapper'>
-          <img className='ban-image' src={draft.redBans[4].icon} alt=''/>
+          <img className='ban-image' src={draft.redBans[2].icon} alt=''/>
+        </span>
+        <span className='ban-image-wrapper'>
+          <img className='ban-image' src={draft.redBans[1].icon} alt=''/>
+        </span>
+        <span className='ban-image-wrapper'>
+          <img className='ban-image' src={draft.redBans[0].icon} alt=''/>
         </span>
       </div>
     )
