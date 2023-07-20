cc2abf96d4791c6df85e6dca619dcc1ba715365c
Barca545 <124243414+Barca545@users.noreply.github.com>
2023-07-19 20:08:22 -0400

    renamed summoner select

---
+++ client/src/Components/BlueSummonerSelect.tsx
@@ -0,0 +1,60 @@
+import {useState, useEffect} from 'react'
+import { BASE_URL,MATCH_ID } from "../App/Slices/baseurl";
+import {Summoner, DraftList} from "../App/Types/champ-select-types";
+import { useWebSocket } from 'react-use-websocket/dist/lib/use-websocket';
+
+//blueside ig I need one for red
+//need to make two player arrays in the draftlist
+export const BlueSummonerSelect = (props:{players:Summoner[],draft:DraftList,champName:string, side:'Blue'|'Red'}) => {
+const [player, setPlayer] = useState(props.players[0])
+const [draft,setDraft] = useState<DraftList>(props.draft)
+
+const {sendMessage} = useWebSocket(`${BASE_URL}/${MATCH_ID}/draft/blueside`, {
+  onOpen: () => console.log('connection opened'),
+  onClose: () => console.log('connection closed'),
+  onMessage: (message:WebSocketEventMap['message']) => {
+    let data:DraftList = JSON.parse(message.data)
+    setDraft(data)
+    console.log(data.bluePicks)
+  },
+  share:true, 
+  retryOnError: true,
+  shouldReconnect: () => true
+  })
+
+  useEffect(()=>{
+    const newDraft = {...draft}
+    const pickIndex = draft.bluePicks.findIndex(pick=>{return pick.champ === props.champName})
+    newDraft.bluePicks[pickIndex].summoner = player
+    sendMessage(JSON.stringify(newDraft))
+  },[player])
+
+const handleNameChange = (name:string) => {
+  const index = props.players.findIndex((player)=>{return player.name===name})
+  setPlayer(props.players[index])
+}
+
+const handleRoleChange = (role:string) => {
+  const newPlayer:Summoner = {name:player.name, role:role}
+  setPlayer(newPlayer)
+}
+
+return(
+  <div className="champ-assignments">
+    <select value={player.name} onChange={(e)=>handleNameChange(e.target.value)}>
+      {props.players.map((player)=>{
+        return(
+          <option key={player.name} value={player.name}>{player.name}</option>
+        )
+      })}
+    </select>
+    <select value={player.role} onChange={(e)=>handleRoleChange(e.target.value)}>
+    {props.players.map((player)=>{
+      return(
+        <option key={player.role} value={player.role}>{player.role}</option>
+      )
+    })}
+    </select>
+  </div>
+)
+}
--- client/src/Components/SummonerSelect.tsx
+++
@@ -1,9 +0,0 @@
-import { Summoner } from "../App/Types/champ-select-types"
-
-export const SummonerSelect = (props:{summoner:Summoner}) =>{
-  return(
-    <select>
-      <option value={props.summoner.name}>{props.summoner.name}</option>
-    </select>
-  )
-}
