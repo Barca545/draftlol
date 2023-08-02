1eb842f3d9c36e8c20492a7a80e6349a77c1708a
Barca545 <124243414+Barca545@users.noreply.github.com>
2023-08-02 03:15:57 -0400

    moved the lock button to a separate component

---
+++ client/src/Components/LockButton.tsx
@@ -0,0 +1,138 @@
+import { useState,useEffect } from "react";
+import { useWebSocket } from "react-use-websocket/dist/lib/use-websocket";
+import { BASE_URL,MATCH_ID } from "../App/Slices/baseurl";
+import { DraftList,isDraft } from "../App/Types/champ-select-types";
+
+export const LockButton = (props:{draft:DraftList,selection:string[]}) => {
+    const {sendMessage} = useWebSocket(`${BASE_URL}/${MATCH_ID}/draft/blueside`, {
+      onOpen: () => console.log('connection opened'),
+      onClose: () => console.log('connection closed'),
+      share:true, 
+      retryOnError: true,
+      shouldReconnect: () => true
+  })
+    
+    const handleConfirm = () => {
+      ///needs to increment pick index
+      if (isDraft(props.draft)){
+        const newDraft:DraftList = {...props.draft,
+          champList:[...props.draft.champList.filter((item)=>item[0]!==props.selection[0])],
+          topList: [...props.draft.topList.filter((item)=>item[0]!==props.selection[0])],
+          jgList:[...props.draft.jgList.filter((item)=>item[0]!==props.selection[0])],
+          midList:[...props.draft.midList.filter((item)=>item[0]!==props.selection[0])],
+          bottomList:[...props.draft.bottomList.filter((item)=>item[0]!==props.selection[0])],
+          supportList:[...props.draft.supportList.filter((item)=>item[0]!==props.selection[0])]
+        }
+        //there is a disconnect between this and the handleSelect
+        switch(props.draft.turnNumber) {
+          case 0: {
+            newDraft.turn = 'Red'
+            newDraft.turnNumber = props.draft.turnNumber+1
+            break
+          }
+          case 1: {
+            newDraft.turn = 'Blue'
+            newDraft.turnNumber = props.draft.turnNumber+1
+            break
+          }
+          case 2: {
+            newDraft.turn = 'Red'
+            newDraft.turnNumber = props.draft.turnNumber+1
+            break
+          }
+          case 3: {
+            newDraft.turn = 'Blue'
+            newDraft.turnNumber = props.draft.turnNumber+1
+            break
+          }
+          case 4: {
+            newDraft.turn = 'Red'
+            newDraft.turnNumber = props.draft.turnNumber+1
+            break
+          }
+          case 5: {
+            newDraft.turn = 'Blue'
+            newDraft.turnNumber = props.draft.turnNumber+1
+            break
+          }
+          case 6: {
+            newDraft.turn = 'Red'
+            newDraft.turnNumber = props.draft.turnNumber+1
+            break
+          }
+          case 7: {
+            newDraft.turn = 'Red'
+            newDraft.turnNumber = props.draft.turnNumber+1
+            break
+          }
+          case 8: {
+            newDraft.turn = 'Blue'
+            newDraft.turnNumber = props.draft.turnNumber+1
+            break
+          }
+          case 9: {
+            newDraft.turn = 'Blue'
+            newDraft.turnNumber = props.draft.turnNumber+1
+            break
+          }
+          case 10: {
+            newDraft.turn = 'Red'
+            newDraft.turnNumber = props.draft.turnNumber+1
+            break
+          }
+          case 11: {
+            newDraft.turn = 'Red'
+            newDraft.turnNumber = props.draft.turnNumber+1
+            break
+          }
+          case 12: {
+            newDraft.turn = 'Blue'
+            newDraft.turnNumber = props.draft.turnNumber+1
+            break
+          }
+          case 13: {
+            newDraft.turn = 'Red'
+            newDraft.turnNumber = props.draft.turnNumber+1
+            break
+          }
+          case 14: {
+            newDraft.turn = 'Blue'
+            newDraft.turnNumber = props.draft.turnNumber+1
+            break
+          }
+          case 15: {
+            newDraft.turn = 'Red'
+            newDraft.turnNumber = props.draft.turnNumber+1
+            break
+          }
+          case 16: {
+            newDraft.turn = 'Blue'
+            newDraft.turnNumber = props.draft.turnNumber+1
+            break
+          }
+          case 17: {
+            newDraft.turn = 'Blue'
+            newDraft.turnNumber = props.draft.turnNumber+1
+            break
+          }
+          //turn 18 should be red but is toggling the last blueban
+          case 18: {
+            newDraft.turn = 'Red'
+            newDraft.turnNumber = props.draft.turnNumber+1
+            break
+          }
+          case 19: {
+            newDraft.turn = 'Done'
+            newDraft.turnNumber = props.draft.turnNumber+1
+            break
+          }
+        }
+        //setOutgoingDraft(newDraft)
+        sendMessage(JSON.stringify(newDraft))
+        
+      }
+    }
+    return (
+      <input className="lock-button" value={'LOCK IN'} type="button" onClick={()=>handleConfirm()}/>
+    )
+  }
--- client/src/Components/LockIn.tsx
+++ client/src/Components/LockIn.tsx
@@ -1,146 +1,39 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import { useWebSocket } from "react-use-websocket/dist/lib/use-websocket";
 import { BASE_URL,MATCH_ID } from "../App/Slices/baseurl";
 import { DraftList,isDraft } from "../App/Types/champ-select-types";
 import { RoleSelect } from "./RoleSelect";
+import { Timer } from "../App/Types/champ-select-types";
+import { LockButton } from "./LockButton";
 
 export const LockIn = (props:{selection:string[],draft:DraftList, side:'Blue'|'Red'|'Done'}) => {
   const [show,setShow] = useState(false)
   const [confirmedDraft, setConfirmedDraft] = useState(false)
+  const [time, setTime] = useState<Timer|null>(null)
 
-  const {sendMessage} = useWebSocket(`${BASE_URL}/${MATCH_ID}/draft/blueside`, {
-      onOpen: () => console.log('connection opened'),
-      onClose: () => console.log('connection closed'),
-      share:true, 
-      retryOnError: true,
-      shouldReconnect: () => true
-      })
-
-  const handleConfirm = () => {
-      ///needs to increment pick index
-      if (isDraft(props.draft)){
-        const newDraft:DraftList = {...props.draft,
-          champList:[...props.draft.champList.filter((item)=>item[0]!==props.selection[0])],
-          topList: [...props.draft.topList.filter((item)=>item[0]!==props.selection[0])],
-          jgList:[...props.draft.jgList.filter((item)=>item[0]!==props.selection[0])],
-          midList:[...props.draft.midList.filter((item)=>item[0]!==props.selection[0])],
-          bottomList:[...props.draft.bottomList.filter((item)=>item[0]!==props.selection[0])],
-          supportList:[...props.draft.supportList.filter((item)=>item[0]!==props.selection[0])]
-        }
-        //there is a disconnect between this and the handleSelect
-        switch(props.draft.turnNumber) {
-          case 0: {
-            newDraft.turn = 'Red'
-            newDraft.turnNumber = props.draft.turnNumber+1
-            break
-          }
-          case 1: {
-            newDraft.turn = 'Blue'
-            newDraft.turnNumber = props.draft.turnNumber+1
-            break
-          }
-          case 2: {
-            newDraft.turn = 'Red'
-            newDraft.turnNumber = props.draft.turnNumber+1
-            break
-          }
-          case 3: {
-            newDraft.turn = 'Blue'
-            newDraft.turnNumber = props.draft.turnNumber+1
-            break
-          }
-          case 4: {
-            newDraft.turn = 'Red'
-            newDraft.turnNumber = props.draft.turnNumber+1
-            break
-          }
-          case 5: {
-            newDraft.turn = 'Blue'
-            newDraft.turnNumber = props.draft.turnNumber+1
-            break
-          }
-          case 6: {
-            newDraft.turn = 'Red'
-            newDraft.turnNumber = props.draft.turnNumber+1
-            break
-          }
-          case 7: {
-            newDraft.turn = 'Red'
-            newDraft.turnNumber = props.draft.turnNumber+1
-            break
-          }
-          case 8: {
-            newDraft.turn = 'Blue'
-            newDraft.turnNumber = props.draft.turnNumber+1
-            break
-          }
-          case 9: {
-            newDraft.turn = 'Blue'
-            newDraft.turnNumber = props.draft.turnNumber+1
-            break
-          }
-          case 10: {
-            newDraft.turn = 'Red'
-            newDraft.turnNumber = props.draft.turnNumber+1
-            break
-          }
-          case 11: {
-            newDraft.turn = 'Red'
-            newDraft.turnNumber = props.draft.turnNumber+1
-            break
-          }
-          case 12: {
-            newDraft.turn = 'Blue'
-            newDraft.turnNumber = props.draft.turnNumber+1
-            break
-          }
-          case 13: {
-            newDraft.turn = 'Red'
-            newDraft.turnNumber = props.draft.turnNumber+1
-            break
-          }
-          case 14: {
-            newDraft.turn = 'Blue'
-            newDraft.turnNumber = props.draft.turnNumber+1
-            break
-          }
-          case 15: {
-            newDraft.turn = 'Red'
-            newDraft.turnNumber = props.draft.turnNumber+1
-            break
-          }
-          case 16: {
-            newDraft.turn = 'Blue'
-            newDraft.turnNumber = props.draft.turnNumber+1
-            break
-          }
-          case 17: {
-            newDraft.turn = 'Blue'
-            newDraft.turnNumber = props.draft.turnNumber+1
-            break
-          }
-          //turn 18 should be red but is toggling the last blueban
-          case 18: {
-            newDraft.turn = 'Red'
-            newDraft.turnNumber = props.draft.turnNumber+1
-            break
-          }
-          case 19: {
-            newDraft.turn = 'Done'
-            newDraft.turnNumber = props.draft.turnNumber+1
-            break
-          }
-        }
-        //setOutgoingDraft(newDraft)
-        sendMessage(JSON.stringify(newDraft))
-        
-      }
-    }
+  const {sendMessage} = useWebSocket(`${BASE_URL}/${MATCH_ID}/timer`, {
+    onOpen: () => console.log('connection opened'),
+    onClose: () => console.log('connection closed'),
+    onMessage: (message:WebSocketEventMap['message']) => {
+      let timer:Timer = JSON.parse(message.data)
+      setTime(timer)
+    },
+    share:false, 
+    retryOnError: true,
+    shouldReconnect: () => true
+  })
+  
+  //this seems to be able to tap into the websocket for the timer. 
+  //Have this one also handle the reset on turn change
+  //just need to make it so if can execute the handle reset
+  useEffect(() => {
+    if(time?.seconds===0){console.log('confirmed time is 0')}
+  },[time])
 
     if (isDraft(props.draft)) {
       if(props.draft.turn!=='Done'){
         return(
-          <input className="lock-button" value={'LOCK IN'} type="button" onClick={()=>handleConfirm()}/>
+          <LockButton draft={props.draft} selection={props.selection}/>
         )
       }
       else{ 
